ed1c2a2cd502fa685e69556d9f86c6997ed27991
Cristian Salazar Arenas <cristiancamilosalazararenas@outlook.com>
2025-05-05 13:34:17 -0500

    Se agregó la documentación de programas en Java para mantener las buenas practicas. En resumen, se agregaron comentarios a la clases: RelojConcurrente y Tiempo.

--- src/autonoma/reloj/main/RelojConcurrente.java
+++ src/autonoma/reloj/main/RelojConcurrente.java
@@ -1,48 +1,79 @@
+//Este el paquete donde su ubica la clase RelojConcurrente.
 package autonoma.reloj.main;
-
+//Importamos la clase Tiempo para poder usar los hilos correctamente.
 import autonoma.reloj.models.Tiempo;
-
+/**
+ * Clase encargada de crear una instancia de la clase Tiempo y de usar hilos para poder representar el reloj.
+ * 
+ * @author      Cristian Camilo Salazar Arenas
+ * @version     1.0
+ * @since       2025.05.04 
+ */
 public class RelojConcurrente {
-
+    //Clase principal que contiene el método main
     public static void main(String[] args) {
+        //Se crea una instancia de la clase Tiempo llamada 'tiempo'
         Tiempo tiempo = new Tiempo();
-
+        //Creamos una instancia de la clase Hilo que se encargará
+        //de manejar los segundos usando una expresión lambda
         Thread segundos = new Thread(() -> {
+            //Creamos un bucle infinito para que el reloj nunca pare
+            //de funcionar hasta que cerremos el programa
             while (true) {
                 try {
-                    Thread.sleep(1000);
+                    //Pausa el hilo cada segundo
+                    Thread.sleep(1000);//Estos son 1000 milisegundos
+                    //Llama a un método de la clase Tiempo para incrementar los segundos
                     tiempo.incrementarSegundos();
+                    //Llama a otro método de la clase Tiempo para mostrar el tiempo actual
                     tiempo.mostrar();
+                    //Maneja posibles interrupciones del hilo
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
-
+        //Creamos otra instancia de la clase Hilo que se encargará
+        //de manejar los minutos usando una expresión lambda
         Thread minutos = new Thread(() -> {
+            //Creamos otro bucle infinito para que el reloj nunca pare
+            //de funcionar hasta que cerremos el programa
             while (true) {
                 try {
+                    //Pausa el hilo cada '60' segundos
                     Thread.sleep(60000);
+                    //Llama a un método de la clase Tiempo para incrementar los minutos
                     tiempo.incrementarMinutos();
+                //Maneja posibles interrupciones del hilo
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
 
+        //Creamos otra instancia de la clase Hilo que se encargará
+        //de manejar las horas usando una expresión lambda
         Thread horas = new Thread(() -> {
+            //Creamos otro bucle infinito para que el reloj nunca pare
+            //de funcionar hasta que cerremos el programa
             while (true) {
                 try {
-                    Thread.sleep(3600000);
+                    //Pausa el hilo cada 3600 segundos
+                    Thread.sleep(3600000); //Estos son milisegundos
+                    //Llama a un método de la clase Tiempo para incrementar las horas
                     tiempo.incrementarHoras();
+                    //Maneja posibles interrupciones del hilo
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
 
+        //Aquí inicializamos el hilo 'segundos' =P
         segundos.start();
+        //Aquí inicializamos el hilo 'minutos'
         minutos.start();
+        //Por último inicializamos el hilo 'horas'
         horas.start();
     }
 }
--- src/autonoma/reloj/models/Tiempo.java
+++ src/autonoma/reloj/models/Tiempo.java
@@ -1,33 +1,65 @@
+//Este es el paquete donde se ubica la clase Tiempo.
 package autonoma.reloj.models;
-
+/**
+ * Clase encargada de aumentar los segundos, minutos y horas de un reloj.
+ * Esta clase simula el paso del tiempo.
+ * 
+ * @author      Cristian Camilo Salazar Arenas
+ * @version     1.0
+ * @since       2025.05.04
+ */
 public class Tiempo {
-
+    /**
+     * Representa la cantidad de horas y se inicializa desde '0'.
+     */
     private int horas = 0;
+    /**
+     * Representa la cantidad de minutos y se inicializa desde '0'.
+     */
     private int minutos = 0;
+    /**
+     * Representa la cantidad de segundos y se inicializa desde '0'.
+     */
     private int segundos = 0;
-
-    public synchronized void incrementarSegundos() {
+    /**
+     * Método encargado de incrementar los segundos del reloj.
+     * 
+     * @since 2025.05.04
+     */
+    public void incrementarSegundos() {
         segundos++;
         if (segundos >= 60) {
             segundos = 0;
         }
     }
-
-    public synchronized void incrementarMinutos() {
+    /**
+     * Método encargado de incrementar los minutos del reloj.
+     * 
+     * @since 2025.05.04
+     */
+    public void incrementarMinutos() {
         minutos++;
         if (minutos >= 60) {
             minutos = 0;
         }
     }
-
-    public synchronized void incrementarHoras() {
+    /**
+     * Método encargado de incrementar las horas del reloj.
+     * 
+     * @since 2025.05.04
+     */
+    public void incrementarHoras() {
         horas++;
         if (horas >= 24) {
             horas = 0;
         }
     }
-
-    public synchronized void mostrar() {
-        System.out.printf("Hora actual: %02d:%02d:%02d%n", horas, minutos, segundos);
+    /**
+     * Método encargado de mostrar las [Horas],[Minutos],[Segundos] del reloj.
+     * 
+     * @since 2025.05.04
+     */
+    public void mostrar() {
+        System.out.printf("Hora actual: " + horas + ":" + minutos + ":" + segundos + "\n");
     }
 }
